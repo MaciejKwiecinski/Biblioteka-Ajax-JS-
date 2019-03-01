9cec94dfd0907ae7470f466a6440a068f21158bc
MaciejKwiecinski <mackwiecinski@gmail.com>
2019-03-01 15:12:21 +0100

    zadanie 4

--- cokolwiek/app.js
+++ cokolwiek/app.js
@@ -1,19 +1,28 @@
-$(document).ready(function () {
+$(document).ready(function() {
+
     $.ajax({
-       method: 'GET',
-       url:'http://127.0.0.1:8000/book/'
-    }).done(function(result){
-      setBooks(result);
+        method: "GET",
+        url: "http://127.0.0.1:8000/book/"
+    }).done(function(result) {
+        setBooks(result);
     });
+
     function setBooks(result) {
-        for(var i = 0; i<result.length;i++){
-            $('.booksList').append('<li><span>'+ result[i].title+'</span><div></div></li>');
-        }
+        for (var i = 0; i < result.length; i++) {
+            $(".booksList")
+            .append("<li><span class='title' data-id='" + result[i].id + "'>" + result[i].title + "</span><div class='details'></div></li>");
+        };
+
+        $(".title").click(function(event) {
+            if ($(this).next().css("display") === "none") {
+                $(this).next().css("display", "block");
+            } else {
+                $(this).next().css("display", "none");
+            }
+        });
     }
 
+});
 
 
 
-
-
-})
